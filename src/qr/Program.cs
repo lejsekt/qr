2a87f852a26af54eac2abf69c36e674e62eaeb8e
Tadeáš Lejsek <tadeas.lejsek@datavision.cz>
2023-12-10 22:07:45 +0100

    Add text to svg qr code command

---
+++ src/qr/Program.cs
@@ -0,0 +1,52 @@
+﻿using System.CommandLine;
+using System.CommandLine.Invocation;
+using Net.Codecrete.QrCodeGenerator;
+using qr.Shared;
+
+var rootCommand = new RootCommand("Create QR code. SVG output will be printed to stdout.");
+
+var textArgument = new Argument<string>("Text", "Text to encode");
+
+var eccOption = new Option<EccMode>("--ecl", "Error correction level");
+eccOption.AddAlias("-e");
+eccOption.SetDefaultValue(EccMode.Medium);
+
+var borderOption = new Option<int>("--border", "Border thickness around QR code");
+borderOption.AddAlias("-m");
+borderOption.SetDefaultValue(4);
+
+rootCommand.AddArgument(textArgument);
+rootCommand.AddOption(eccOption);
+rootCommand.AddOption(borderOption);
+rootCommand.SetHandler((text, eccMode, border, ctx) =>
+{
+    try
+    {
+        var ecc = eccMode switch
+        {
+            EccMode.Low => QrCode.Ecc.Low,
+            EccMode.Medium => QrCode.Ecc.Medium,
+            EccMode.High => QrCode.Ecc.High,
+            EccMode.Quartile => QrCode.Ecc.Quartile,
+            _ => throw new ArgumentException($"Unsupported ECL value '{eccMode.ToString()}'")
+        };
+        var qr = QrCode.EncodeText(text, ecc);
+        var svg = qr.ToSvgString(border);
+        ctx.Console.WriteLine(svg);
+    }
+    catch (Exception ex)
+    {
+        ctx.Console.WriteLine($"Failed to create QR code: {ex.Message}");
+        ctx.ExitCode = 1;
+    }
+}, textArgument, eccOption, borderOption, Bind.FromServiceProvider<InvocationContext>());
+
+return rootCommand.Invoke(args);
+
+internal enum EccMode
+{
+    Low,
+    Medium,
+    High,
+    Quartile
+}
---
+++ src/qr/Shared/Bind.cs
@@ -0,0 +1,15 @@
+using System.CommandLine.Binding;
+
+namespace qr.Shared;
+
+internal static class Bind
+{
+    public static ServiceProviderBinder<T> FromServiceProvider<T>() => ServiceProviderBinder<T>.Instance;
+
+    internal class ServiceProviderBinder<T> : BinderBase<T>
+    {
+        public static ServiceProviderBinder<T> Instance { get; } = new();
+
+        protected override T GetBoundValue(BindingContext bindingContext) => (T) bindingContext.GetService(typeof(T))!;
+    }
+}
