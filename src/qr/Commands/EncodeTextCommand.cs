0d911a33c9f3e3d0da322f75eddf29ae0eafb54e
Tadeáš Lejsek <tadeas.lejsek@datavision.cz>
2023-12-11 21:45:34 +0100

    Extract encode text command

---
+++ src/qr/Commands/EncodeTextCommand.cs
@@ -0,0 +1,38 @@
+using System.CommandLine;
+using System.CommandLine.Invocation;
+using Net.Codecrete.QrCodeGenerator;
+using qr.Shared;
+
+namespace qr.Commands;
+
+public static class EncodeTextCommand
+{
+    public static RootCommand Create()
+    {
+        var rootCommand = new RootCommand("Create QR code. SVG output will be printed to stdout.");
+
+        var textArgument = new Argument<string>("Text", "Text to encode");
+        var eccOption = OptionsFactory.CreateEccOption();
+        var borderOption = OptionsFactory.CreateBorderOption();
+
+        rootCommand.AddArgument(textArgument);
+        rootCommand.AddOption(eccOption);
+        rootCommand.AddOption(borderOption);
+        rootCommand.SetHandler((text, eccMode, border, ctx) =>
+        {
+            try
+            {
+                var ecc = EccMapper.From(eccMode);
+                var qr = QrCode.EncodeText(text, ecc);
+                var svg = qr.ToSvgString(border);
+                ctx.Console.WriteLine(svg);
+            }
+            catch (Exception ex)
+            {
+                ctx.Console.WriteLine($"Failed to create QR code: {ex.Message}");
+                ctx.ExitCode = 1;
+            }
+        }, textArgument, eccOption, borderOption, Bind.FromServiceProvider<InvocationContext>());
+        return rootCommand;
+    }
+}
--- src/qr/Program.cs
+++ src/qr/Program.cs
@@ -1,31 +1,6 @@
 ﻿using System.CommandLine;
-using System.CommandLine.Invocation;
-using Net.Codecrete.QrCodeGenerator;
-using qr.Shared;
+using qr.Commands;
 
-var rootCommand = new RootCommand("Create QR code. SVG output will be printed to stdout.");
-
-var textArgument = new Argument<string>("Text", "Text to encode");
-var eccOption = OptionsFactory.CreateEccOption();
-var borderOption = OptionsFactory.CreateBorderOption();
-
-rootCommand.AddArgument(textArgument);
-rootCommand.AddOption(eccOption);
-rootCommand.AddOption(borderOption);
-rootCommand.SetHandler((text, eccMode, border, ctx) =>
-{
-    try
-    {
-        var ecc = EccMapper.From(eccMode);
-        var qr = QrCode.EncodeText(text, ecc);
-        var svg = qr.ToSvgString(border);
-        ctx.Console.WriteLine(svg);
-    }
-    catch (Exception ex)
-    {
-        ctx.Console.WriteLine($"Failed to create QR code: {ex.Message}");
-        ctx.ExitCode = 1;
-    }
-}, textArgument, eccOption, borderOption, Bind.FromServiceProvider<InvocationContext>());
+var rootCommand = EncodeTextCommand.Create();
 
 return rootCommand.Invoke(args);
